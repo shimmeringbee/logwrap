fc74fc07ef09d00e5b53b5ce241ac11c9e990cf7
Peter Wood <peter@alastria.net>
2020-07-12 11:44:09 +0100

    Tweak explanation.

--- option_sourcetrace.go
+++ option_sourcetrace.go
@@ -18,7 +18,7 @@ const entrypointFunctionPrefix = "github.com/shimmeringbee/logwrap.Logger"
 
 // SourceTrace inserts the file and line number of the file that Log was called at. This routine searches for the frame
 // pointer before the first call to the Logger object. The search is used rather than static in case the option is used
-// in a post option filter.
+// in a post option filter, and to allow utility wrappers such as LogInfo instead of `Log(...,...,Level(Info))`.
 func SourceTrace(message *Message) {
 	programCounters := make([]uintptr, maximumInternalFrameDepth)
 	n := runtime.Callers(0, programCounters)
